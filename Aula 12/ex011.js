56841b9d55d64b80c46fcb0da73dac627a08e578
Abiel Pisa <abielpisa@gmail.com>
2020-04-17 11:52:44 -0300

    atualização

--- Aula 12/ex011.js
+++ Aula 12/ex011.js
@@ -2,7 +2,7 @@ var idade = 13
 if (idade < 16){
     console.log('Não vota!')
 }else{
-    if (idade >= 16 && idade < 18){
+    if (idade < 18){
         console.log('Voto opcional!')
     }else{
         console.log('Voto Obrigatório!')
