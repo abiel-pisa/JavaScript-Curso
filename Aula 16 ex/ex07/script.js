1fbdc4f52300805223bbebd14e40994e2c003a64
Abiel Pisa <abielpisa@gmail.com>
2020-04-30 21:41:08 -0300

    bro

---
+++ Aula 16 ex/ex07/script.js
@@ -0,0 +1,29 @@
+let num = document.getElementById('txtnum')
+let lista = document.getElementById('flista')
+let res = document.getElementById('res')
+let valores=[]
+
+function isNumero(n){
+    if(Number(n) >= 1 && Number(n) <= 100){
+        return true
+    }else{
+        return false
+    }
+}
+
+function inLista(n, l){
+    if(l.indexOf(Number(n)) != -1){
+        return true
+    }else{
+        return false
+    }
+
+}
+
+function adicionar(){
+    if(isNumero(num.value) && !inLista(num.value, valores)){
+        window.alert('Tudo OK!')
+    }else{
+        window.alert('Valor invalido ou já encontrado na lista.')
+    }
+}
